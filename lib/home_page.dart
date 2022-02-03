74312fbca283914cb469b0ad8714d8613a8b8da5
Declan <2038796D@student.gla.ac.uk>
2022-02-03 19:51:04 +0000

    Shared prefs added

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_sodium/flutter_sodium.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,14 +12,29 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin{
+  late SharedPreferences sharedPreferences;
   late TabController _tabController;
-  List<Tab> _tabs = [Tab(child: Text("Messages", style: GoogleFonts.robotoMono())), Tab(child: Text("Private messages"))]; //Right now these aren't correctly changed, they have to be manuially done. Maybe we need to set different styles in main.dart?
+  String identity = "Author";
+  List<Tab> _tabs = [Tab(child: Text("Messages", style: GoogleFonts.robotoMono())), Tab(child: Text("Private messages", style: GoogleFonts.robotoMono()))]; //Right now these aren't correctly changed, they have to be manuially done. Maybe we need to set different styles in main.dart?
   List<dynamic> messages = [];
   List<dynamic> privateMessages = [];
 
   @override
-  void initState() {
+  void initState() async {
     super.initState();
+    sharedPreferences = await SharedPreferences.getInstance();
+
+    if(!(sharedPreferences.containsKey("identity")) || !(sharedPreferences.containsKey("privateKey"))){
+      Sodium.init();
+      KeyPair keyPair = Sodium.cryptoSignSeedKeypair((RandomBytes.buffer(32)));
+      String identity = base64Encode(keyPair.pk);
+
+      sharedPreferences.setString("identity", "@$identity.ed25519");
+      sharedPreferences.setString("privateKey", base64Encode(keyPair.pk));
+    }
+
+    String? identity = sharedPreferences.getString("identity");
+    this.identity = identity!;
     _tabController = TabController(vsync: this, length: _tabs.length);
   }
 
@@ -48,14 +66,14 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
                 Card(
                   child: Column(
                     children: [
-                      const ListTile(
+                      ListTile(
                         leading: Icon(Icons.person_rounded),
-                        title: Text("Author here"),
+                        title: Text(identity),
                         subtitle: Text("x hours ago")
                       ),
                       Container(
                         padding: EdgeInsets.all(16),
-                        child: Text("A message would go here. Something like LALALALALALALALALALA here's some really cool interesting message!A message would go here. Something like LALALALALALALALALALA here's some really cool interesting message!A message would go here. Something like LALALALALALALALALALA here's some really cool interesting message!A message would go here. Something like LALALALALALALALALALA here's some really cool interesting message!A message would go here. Something like LALALALALALALALALALA here's some really cool interesting message!A message would go here. Something like LALALALALALALALALALA here's some really cool interesting message!A message would go here. Something like LALALALALALALALALALA here's some really cool interesting message!A message would go here. Something like LALALALALALALALALALA here's some really cool interesting message!A message would go here. Something like LALALALALALALALALALA here's some really cool interesting message!A message would go here. Something like LALALALALALALALALALA here's some really cool interesting message!A message would go here. Something like LALALALALALALALALALA here's some really cool interesting message!A message would go here. Something like LALALALALALALALALALA here's some really cool interesting message!"),
+                        child: Text("A message would go here. Something like LALALALALALALALALALA here's some really cool interesting things I want to say!"),
                       ),
                     ],
                   ),
