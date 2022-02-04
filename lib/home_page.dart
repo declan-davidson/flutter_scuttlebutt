2d4da76fe38382b70bdccb9f6acfaacf3bb6575a
Declan <2038796D@student.gla.ac.uk>
2022-02-04 17:02:42 +0000

    Feed update applied

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -38,9 +38,9 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
     if(identity != null && encodedSk != null){
       await FeedService.postMessage(rng.nextInt(1000).toString(), identity, encodedSk);
       List<dynamic> messages = await FeedService.retrieveMessages(identity: identity);
+      lastMessageBody = messages.last.content["content"] ?? "Nothing was returned";
       
       setState(() {
-        lastMessageBody = messages.last.content["content"] ?? "Nothing was returned";
       });
     }
     else{
